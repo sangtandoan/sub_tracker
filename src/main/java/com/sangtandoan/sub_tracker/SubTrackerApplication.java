d2c66998ee5152a9bb493edfbdd581c34c743abe
sangtandoan <sangvaminh11497@gmail.com>
2025-06-14 21:02:54 +0700

    feat: setup

---
+++ src/main/java/com/sangtandoan/sub_tracker/SubTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.sangtandoan.sub_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sangtandoan/sub_tracker/SubTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangtandoan.sub_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
