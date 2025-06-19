9539719ca07e6516697164351fcb5914a36d12d7
sangtandoan <sangvaminh11497@gmail.com>
2025-06-19 13:32:36 +0700

    fix: changes packge of AccessDeniedException

--- src/main/java/com/sangtandoan/sub_tracker/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/sangtandoan/sub_tracker/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.sangtandoan.sub_tracker.exceptions;
 
-import java.nio.file.AccessDeniedException;
+import org.springframework.security.access.AccessDeniedException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
